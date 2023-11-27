4a0bdddd398b5099b04cf009a613b0d226d04fcd
Amine <mezzatamine2002@gmail.com>
2023-11-27 06:12:52 +0100

    creating and mapping the pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Image from 'next/image'
+import fs from "fs"
+import Link from 'next/link';
+
+
+const getBlogPost = () => {
+  const folder = "blogposts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const titles = markdownPosts.map((file) => file.replace(".md", ""));
+  return titles;
+}
 
 export default function Home() {
+  const metaData = getBlogPost();
+  const postPreviews = metaData.map((title) => (
+    <div>
+      <Link href={`/blogPosts/${title}`}>
+      <h2>{title}</h2>
+      </Link>
+    </div>
+  ));
   return (
-    <main className="">
-
+    <main>
+      {postPreviews}
     </main>
   )
 }
